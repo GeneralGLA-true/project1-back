fe61233382554eeb2bb1d78b5c9be39dda65acc4
GeneralGLA-true <artemstarodub@gmail.com>
2024-01-17 20:50:04 +0200

    task1-module5-back

--- server.js
+++ server.js
@@ -2,28 +2,23 @@ const http = require('http');
 const fs = require('fs');
 const PORT = 3000;
 
-
 const server = http.createServer((req, res) => {
     console.log('Server request');
     console.log(req.url, req.method);
-
-
     const url = req.url;
 
     if(url === '/photos'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain, charset=utf-8;')
+        res.writeHead(200, {
+            'Content-Type': 'text/plain',
+            'Access-Control-Allow-Origin' : '*'
+          });
         fs.readFile("mockData.txt", function(error,data){
             if(error) {
                 return console.log(error);
             }
             res.end(data);
         });
-
-    } else {
-        res.write('Hello world');
-        res.end('');
-    }
+    };
 });
 
 server.listen(PORT, 'localhost', (error) => {
