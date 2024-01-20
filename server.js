2908357218da6e53ed8afbfbbda125c2afe84764
GeneralGLA-true <artemstarodub@gmail.com>
2024-01-20 19:48:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const PORT = 3000;
+const PORT = 3001;
 
 const server = http.createServer((req, res) => {
     console.log('Server request');
